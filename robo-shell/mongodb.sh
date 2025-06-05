5afe1f5c06f93b9cb3b0ecf8ac3d0adaa3ac2c67
arunjesusdevops <postgresarun@gmail.com>
2025-06-05 19:08:11 +0530

    This is my first script -v4

--- robo-shell/mongodb.sh
+++ robo-shell/mongodb.sh
@@ -40,6 +40,6 @@ sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
 
 VALIDATE $? "Remote access to MongoDB"
 
-systemctl restart mongod
+systemctl restart mongod &>> $LOGFILE
 
 VALIDATE $? "Restarting MongoDB"
